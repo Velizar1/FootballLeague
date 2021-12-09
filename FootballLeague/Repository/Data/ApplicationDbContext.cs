c0568a154141c17bcb5878a12e3043cb9c49ea87
v.d.vrubchev <v.d.vrubchev@gmail.com>
2021-12-09 18:59:24 +0200

    Init

---
+++ FootballLeague/Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
