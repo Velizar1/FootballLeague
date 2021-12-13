f1c147b7cf528599466d5cf6508f3a84abacf4ee
v.d.vrubchev <v.d.vrubchev@gmail.com>
2021-12-13 14:28:48 +0200

    Modify structure

--- FootballLeague/FootballLeague.DataAccess/Data/ApplicationDbContext.cs
+++ FootballLeague/FootballLeague.DataAccess/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Repository.Data
+namespace FootballLeague.DataAccess.Data
 {
     public class ApplicationDbContext:DbContext
     {
